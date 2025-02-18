452cdbdfdbc662e82a792d0e65aa19e91e2e01ee
nikhilyt01 <nikhil0808yt@gmail.com>
2025-02-18 23:59:46 +0530

    renamed frontned to frontend

--- Frontned/src/config.ts
+++
@@ -1,8 +0,0 @@
-
-export const Backend_url =import.meta.env.VITE_BACKEND_URL
-
-export const Front_url=import.meta.env.VITE_FRONT_URL
-
-
-
-
--- Frontned/src/icons/index.ts
+++
@@ -1,9 +0,0 @@
-export interface iconprops {
-    size:"sm" | "md" |"lg";
-}
-
-export const iconvariants = {
-    "sm":"size-2",
-    "md": "size-4",
-    "lg":"size-6"
-}
